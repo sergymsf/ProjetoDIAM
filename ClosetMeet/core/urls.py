b6e778b6d8d055e11f7384e1a12b1894fe9feeaa
sergymsf <sergio.svcl@gmail.com>
2024-05-10 21:20:52 +0100

    1

--- ClosetMeet/core/urls.py
+++ ClosetMeet/core/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('login', views.login_handler, name='login_handler'),
     path('logout', views.logout_handler, name='logout_handler'),
     path('signin', views.signin_handler, name='signin_handler'),
-    path('personalpage/', views.personal_page, name='personal_page')
+    path('personal-page/', views.personal_page, name='personal_page'),
+    path('personal-page/edit-profile', views.edit_profile, name='edit_profile'),
+    path('personal-page/add-clothing-item', views.add_clothing_item, name='add_clothing_item'),
     ]
--- ClosetMeet/core/views.py
+++ ClosetMeet/core/views.py
@@ -1,3 +1,4 @@
+import os
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
@@ -5,7 +6,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 from django.http import Http404, HttpResponse, HttpResponseRedirect, HttpResponseNotFound
 from django.contrib.auth.models import User
-from .models import Profile
+from django.conf import settings
+from .models import ClothingItem, Profile
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login
+from django.core.files.storage import FileSystemStorage
+
 
 def initial_page(request):
     return  render(request, 'initial_page.html')
@@ -13,15 +20,36 @@ def initial_page(request):
 def join_page(request):
     return  render(request, 'join_page.html')
 
+@login_required(login_url=  'core:join_page')
 def personal_page(request):
     dont_has_picture = True
     if request.user.profile.picture:
         dont_has_picture = False
     return render(request, 'personal_page.html', {'dont_has_picture': dont_has_picture})
 
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.contrib.auth import authenticate, login
+@login_required(login_url=  'core:join_page')
+def edit_profile(request):
+    profile = request.user.profile
+
+    if request.method == 'POST':
+        if 'profile-picture' in request.FILES:
+            if profile.picture:
+                file_path = profile.picture.path
+                if os.path.exists(file_path):
+                    os.remove(file_path)
+            username = request.user.username
+            file_extension = ".png"
+            stri = str(username) +file_extension
+            myfile = request.FILES['profile-picture']
+            fs = FileSystemStorage()
+            filename = fs.save(stri, myfile)
+            profile.picture = filename 
+        if 'bio' in request.POST:
+            profile.bio = request.POST['bio']
+        profile.save()
+        return HttpResponseRedirect(reverse('core:personal_page'))
+
+    return render(request, 'edit_page.html', {'profile': profile})
 
 def login_handler(request):
     if request.method == 'POST':
@@ -50,6 +78,7 @@ def login_handler(request):
             return HttpResponse(response)
 
 
+@login_required(login_url=  'core:join_page')
 def logout_handler(request):
     logout(request)
     return HttpResponseRedirect(reverse('core:initial_page'))
@@ -66,5 +95,26 @@ def signin_handler(request):
         return HttpResponseRedirect(reverse('core:initial_page'))
     else:
         return render(request, 'intial_page.html')
+    
+
+@login_required(login_url=  'core:join_page')
+def add_clothing_item(request):
+    if request.method == 'POST':
+        image = request.FILES.get('clothing-image')
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        owner = request.user  
+
+        new_clothing_item = ClothingItem.objects.create(
+            owner=owner,
+            image=image,
+            name=name,
+            description=description,
+            price=price
+        )
+        return redirect('personal_page.html')  # Redirect to the clothing item list page
+    
+    return render(request, 'add_clothing_item.html')
 
 
